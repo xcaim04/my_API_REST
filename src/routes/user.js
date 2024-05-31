cf24c3e44e06c68f942a34091d22ca32007733d0
Carlos Blanco <carlojbm04@gmail.com>
2024-05-31 12:48:03 +0200

    Create CRUD

--- src/routes/user.js
+++ src/routes/user.js
@@ -16,4 +16,54 @@ router.post("/users", (req, res) => {
         .catch(err => console.log(err));
 });
 
+// get all users
+router.get("/users", (req, res) => {
+    userSchema
+        .find()
+        .then((data) => {
+            res.json(data);
+            console.log("Get all users");
+        })
+        .catch(err => console.log(err));
+});
+
+// get a users
+router.get("/users/:id", (req, res) => {
+    const { id } = req.params;
+    userSchema
+        .findById(id)
+        .then((data) => {
+            res.json(data);
+            console.log("Get a user");
+        })
+        .catch(err => console.log(err));
+});
+
+// update a users
+router.put("/users/:id", (req, res) => {
+    const { id } = req.params;
+    const { name, age, email } = req.body;
+    userSchema
+        .updateOne({ _id: id }, { $set: { name, age, email } })
+        .then((data) => {
+            res.json(data);
+            console.log("User has been updated");
+        })
+        .catch(err => console.log(err));
+});
+
+// delete a user
+router.delete("/users/:id", (req, res) => {
+    const { id } = req.params;
+    userSchema
+        .deleteOne({ _id:id})
+        .then((data) => {
+            res.json(data);
+            console.log("User has been deleted");
+        })
+        .catch(err => console.log(err));
+});
+
+
+
 module.exports = router;
